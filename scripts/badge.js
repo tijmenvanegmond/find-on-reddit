0d4017eb6c7422ccba4be219a1fb6b3d0b3aecd2
Tijmen van Egmond <tym_time@hotmail.com>
2018-08-04 01:18:17 +0200

    Just some reformatting

--- scripts/badge.js
+++ scripts/badge.js
@@ -1,19 +1,17 @@
 const DEFAULT_BADGE_COLOUR = "green";
 var Badge = {
-    
-    Reset : function ()
-    {    
+
+    Reset: function () {
         chrome.browserAction.setBadgeText(
-            {text: null}
+            { text: null }
         );
     },
 
-    Set : function(options)
-    {    
+    Set: function (options) {
         chrome.browserAction.setBadgeText(
-            {text: String(options.text)}
+            { text: String(options.text) }
         );
-        
-        chrome.browserAction.setBadgeBackgroundColor({color: options.color || options.colour || DEFAULT_BADGE_COLOUR});
+
+        chrome.browserAction.setBadgeBackgroundColor({ color: options.color || options.colour || DEFAULT_BADGE_COLOUR });
     }
 };
--- scripts/manage-options.js
+++ scripts/manage-options.js
@@ -1,4 +1,4 @@
-const DEFAULT_OPTIONS ={
+const DEFAULT_OPTIONS = {
     autoload: true,
     whitelist: "youtube.com youtu.be reddit.com old.reddit.com",
     loadlimit: 15
@@ -13,36 +13,34 @@ chrome.runtime.onMessage.addListener(function (answer) {
     }
 });
 
-function UpdateSettings(){
+function UpdateSettings() {
     var getting = browser.storage.local.get();
     getting.then(SetSettings);
     Cache.Empty();
     console.log("FIND-ON-REDDIT: updated settings");
 }
 
-function SetSettings(data) 
-{
+function SetSettings(data) {
     data = SetDefaults(data);
     SettingsData = data;
 }
 
-function SetDefaults(data)
-{
+function SetDefaults(data) {
     let hasSetDefault = false;
-    
-    if(data === undefined){
+
+    if (data === undefined) {
         hasSetDefault = true;
         data = DEFAULT_OPTIONS;
     }
 
     for (let propName in DEFAULT_OPTIONS) {
-        if(data[propName] === undefined){
-            hasSetDefault= true;
+        if (data[propName] === undefined) {
+            hasSetDefault = true;
             data[propName] = DEFAULT_OPTIONS[propName];
-        }        
+        }
     }
 
-    if(hasSetDefault){
+    if (hasSetDefault) {
         chrome.storage.local.set(data);
     }
     return data;
